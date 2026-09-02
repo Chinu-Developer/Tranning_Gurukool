ed8af3c3f69001cb5f8cef3c1913339d4ef2d9fc
Chinu <gojoj186@gmail.com>
2026-09-02 17:42:10 +0530

    I did the libray Management System

---
+++ Build_day/Lib_Mgmt_system/MY.py
@@ -0,0 +1,92 @@
+import colorama
+from colorama import  Fore ,Back , Style ,init 
+
+def menu():
+    init(autoreset=True)
+    print(Fore.MAGENTA + '''╔══════════════════════════════════════════════╗
+║                                              ║
+║        📚  LIBRARY MANAGEMENT SYSTEM  📚     ║
+║                                              ║
+╚══════════════════════════════════════════════╝''')
+
+    print(Fore.BLUE +''' 1.Add Book
+2. View Book 
+3.Search BOOK 
+4.Register User
+5.Issue Book 
+6.Exit ''')   
+
+
+
+def readbook():
+    """This is a function which gives you the data of the book"""
+    with open("book.txt", "r") as f:
+        book_ele = f.readlines()
+        return book_ele
+    
+def Viewbook ():
+    """ Thia a view book function this hepls you to view a exact Book """
+    book_ele = readbook ()
+    print(input("Enter the Particular bokk"))
+    print("Done viewing ")
+
+def add_book():
+    """This is a add book Function . It takes Book Id, Book Name, Author,Quantity as input """
+    book_ele  = readbook()
+    print(Fore.GREEN + "--- ADD BOOK ---" )
+    book_name = input("Enter the Book name  :")
+    for i in book_ele:
+        if book_name in i:
+            print(Fore.GREEN + "The Book Already Exists")
+            print(Fore.RED + " Please use Update Function")
+            return 10
+    book_id = len(book_ele) + 1
+
+
+    book_author = input("Enter author Name Please : ")
+    print(Fore.GREEN + "Book Id is :: ", book_id)
+    error_message = Fore.RED + "Invalid quantity entered \n Please Enter a valid Value"
+    while True:
+        book_quantity = input("Enter Quantity :").strip()
+        if book_quantity.isdigit() :
+            if int(book_quantity) > 0:
+                book_quantity = int(book_quantity)
+                break
+            else:
+                print(error_message)    
+        else:
+            print(error_message)
+    print(Fore.GREEN + f'{"Book added successfully.":^100}')
+
+    with  open("book.txt", "a") as f:
+        f.write(f"{book_id}, {book_name}, {book_author}, {book_quantity} \n")
+
+
+if __name__ == "__main__":
+    while True:
+        menu()
+        choice = input("Enter Your Choice From 1 to 6 :: ")
+
+        if choice == "1":
+            add_book()
+        elif choice == "2":
+            print("Viewbook")
+        elif choice == "3":
+            print("Search Book")
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "6":
+            print(Fore.GREEN +" Thankyou For Visiting, Please Visti Aagin ")
+            break
+        else:
+            print(Fore.RED + "Invalid Choice") 
+
+   
+
+          
+
+             
+       
+     
